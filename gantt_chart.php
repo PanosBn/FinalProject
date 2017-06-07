f87296803f63360d67a397f4060a1655f76672ae
panos <panagiotisban@gmail.com>
2017-06-08 02:40:20 +0300

    Fixed Various javascript mishaps

--- gantt_chart.php
+++ gantt_chart.php
@@ -53,11 +53,11 @@ if ($user->session_status()){
                 var start_date = document.createElement("input");
                 var end_date = document.createElement("input");
                 input.type = "text";
-                input.name = "gantt_lines" + i;
+                input.name = "gantt_lines_" + i;
                 start_date.type = "date";
-                start_date.name = "gantt_date" + i;
+                start_date.name = "start_date_" + i;
                 end_date.type = "date";
-                end_date.name = "gantt_date" + i;
+                end_date.name = "end_date_" + i;
                 container.appendChild(input);
                 container.appendChild(document.createTextNode( " Εναρξη: "));
                 container.appendChild(start_date);
@@ -68,10 +68,44 @@ if ($user->session_status()){
             }
             //Dhmiourgia koumpiou kai topothetisi tou katw apo n-osto element pou tha paragei o xristis gia to gantt
             var span = document.createElement("span");
-            span.innerHTML = '<button class="button button-primary" id="button_gantt" onclick="callJavascriptFunction()">Δημιουργία </>';
+            span.innerHTML = '<button class="button button-primary" id="button_gantt" onclick="createGanttCharts()">Δημιουργία </>';
             container.appendChild(span);
             
-        }     
+        }
+
+        function createGanttCharts(){
+          //arxika tha mazepsoume oles tis varialbes apo kathe typo dedomenwn (perigrafi , start date kai end date)
+          //Tha topothetithoun se 3 arrays
+          //Epeidi ta pedia dimiourgountai dunamika den kseroume to plires onoma tous h ton arithmo tous opote tha ta vroume
+          //kanontas xrisi twn Selectors -->  https://www.w3.org/TR/selectors-api/ 
+          //Kathe stoixeio tupou text ksekina me to name = "gantt_lines_ + ena auksonta arithmo"
+          //To idio isxuei kai gia ta stoixeia typou date , start_date_ + arithmos , end_date_ + arithmos
+          var elements_perigrafes = document.querySelectorAll('input[name^="gantt_lines_"]');
+          var elemetns_startDate = document.querySelectorAll('input[name^="start_date_"]');
+          var elemetns_endDate = document.querySelectorAll('input[name^="end_date_"]');
+
+          var perigrafes_array = new Array();
+          var start_date_array = new Array();
+          var end_date_array = new Array();
+
+          for (i = 0; i < elements_perigrafes.length; i++) {
+                //console.log(elements_perigrafes[i].value);
+                perigrafes_array.push(elements_perigrafes[i].value);
+                console.log(perigrafes_array[i]);
+          }
+          for (i = 0; i < elements_perigrafes.length; i++) {
+                //console.log(elemetns_startDate[i].value);
+                start_date_array.push(elemetns_startDate[i].value);
+                console.log(start_date_array[i]);
+          }
+          for (i = 0; i < elements_perigrafes.length; i++) {
+                //console.log(elemetns_endDate[i].value);
+                end_date_array.push(elemetns_endDate[i].value);
+                console.log(end_date_array[i]);
+          }
+
+
+        }
 
 </script>
 
