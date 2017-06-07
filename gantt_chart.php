4c74d4654bef7baf57e8dc865c247ca00e39d8cd
panos <panagiotisban@gmail.com>
2017-06-08 00:27:18 +0300

    Dynamic user input for gantt charts

--- gantt_chart.php
+++ gantt_chart.php
@@ -10,10 +10,8 @@ if ($user->session_status()){
 
 ?>
 
-<script type="text/javascript">
-     
 
-</script>
+
 
 
 
@@ -22,7 +20,10 @@ if ($user->session_status()){
         <div class="container">
             <div class="row">
                 <section class="thesis-list">
-
+                  <label for = "gantt_lines">Εισάγετε τον αριθμό των διαστημάτων του γραφήματος Gantt </label>
+                  <input type="text" id="gantt_lines" name="gantt_lines"><br />
+                  <a href="#" id="arithmos_gantt" onclick="addFields()">Εμφάνιση</a>
+                   <div id="lines"/>
                 </section>
             </div>
         </div>
@@ -35,6 +36,33 @@ if ($user->session_status()){
 </div>
 
 
+<script type="text/javascript">
+        function addFields(){
+            // Number of inputs to create
+            var number = document.getElementById("gantt_lines").value;
+            // Container <div> where dynamic content will be placed
+            var container = document.getElementById("lines");
+            // Clear previous contents of the container
+            while (container.hasChildNodes()) {
+                container.removeChild(container.lastChild);
+            }
+            for (i=0;i<number;i++){
+                // Append a node with a random text
+                container.appendChild(document.createTextNode((i+1) + " "));
+                // Create an <input> element, set its type and name attributes
+                var input = document.createElement("input");
+                var input_date = document.createElement("input_date");
+                input.type = "text";
+                input_date.type = "date";
+                input.name = "gantt_lines" + i;
+                input_date.name = "gantt_date" + i;
+                container.appendChild(input);
+                // Append a line break 
+                container.appendChild(document.createElement("br"));
+            }
+        }     
+
+</script>
 
 
 <div class="blurred" </div>
